475f4422ffc3eb70fb8ded5ce6f4fa4810319298
vnilov <nilov.vadim@gmail.com>
2017-11-19 15:26:09 +0300

    add proc file existing

--- src/classes/Checker.php
+++ src/classes/Checker.php
@@ -6,23 +6,21 @@ class Checker
 
     public static function check()
     {
-        // get last pid from file
-        $pid = file_get_contents(dirname(__FILE__) . '/' . self::PROC_FILE);
-        ob_start();
-        // get current process list
-        system("ps -e | awk '{print $1}'");
-        // set it to an array
-        $system = ob_get_clean();
-        $pids = explode(PHP_EOL, $system);
-        // if there is a live previous process, than do nothing
-        if (in_array($pid, $pids)) {
-            return false;
-        } else {
-            // else, save current process
-            file_put_contents(dirname(__FILE__) . '/' . self::PROC_FILE, getmypid());
-            return true;
+        if (file_exists("config.json")) {
+            // get last pid from file
+            $pid = file_get_contents(dirname(__FILE__) . '/../' . self::PROC_FILE);
+            ob_start();
+            // get current process list
+            system("ps -e | awk '{print $1}'");
+            // set it to an array
+            $system = ob_get_clean();
+            $pids = explode(PHP_EOL, $system);
+            // if there is a live previous process, than do nothing
+            if (in_array($pid, $pids)) {
+                return false;
+            }
         }
-
+        file_put_contents(dirname(__FILE__) . '/' . self::PROC_FILE, getmypid());
+        return true;
     }
-
 }
