1c02b1c5d02c09f4205db72876177eda778a0625
vnilov <nilov.vadim@gmail.com>
2017-11-19 15:27:12 +0300

    add Checker class to autoload

--- src/autoload.php
+++ src/autoload.php
@@ -2,4 +2,5 @@
     include_once "../vendor/autoload.php";
     include_once "classes/S3Config.php";
     include_once "classes/S3Wrapper.php";
+    include_once "classes/Checker.php";
     include_once "classes/Runner.php";
